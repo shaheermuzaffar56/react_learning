85a4839d57f644f620f4817de716465aa5051c31
SHAHEER MUZAFFAR <edewvv@gmail.com>
2026-07-01 12:56:24 +0500

    Update App.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -6,6 +6,19 @@ import Card from './Card'
 
 
 function App() {
+ return (
+    <div className="container">
+      <h1 className="text-center my-4">Product List</h1>
+
+      <div className="row">
+        {products.map(product => (
+          <div className="col-md-4 mb-4" key={product.id}>
+            <Card name={product.name} price={product.price} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App
