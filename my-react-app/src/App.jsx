1c53327e447357291583bc8cb5575670ee004fd9
SHAHEER MUZAFFAR <edewvv@gmail.com>
2026-07-01 17:40:18 +0500

    Restructure the files according to the tasks

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,24 +1,22 @@
 import { useState } from 'react'
 import './App.css'
-import {products} from './data'
-import Card from './Card'
-import StudentManager from './StudentManager'
+import {products} from './Product_listing/Data'
+import Card from './Product_listing/Card'
+import StudentManager from './Student_CRUD/StudentManager'
 
 
 function App() {
  return (
-    // <div className="container">
-    //   <h1 className="text-center my-4">Product List</h1>
+    <div className="container">
+      <h1 className="text-center my-4">Product List</h1>
 
-    //   <div className="row">
-    //     {products.map(product => (
-    //       <div className="col-md-4 mb-4" key={product.id}>
-    //         <Card name={product.name} price={product.price} />
-    //       </div>
-    //     ))}
-    //   </div>
-    // </div>
-    <div>
+      <div className="row">
+        {products.map(product => (
+          <div className="col-md-4 mb-4" key={product.id}>
+            <Card name={product.name} price={product.price} />
+          </div>
+        ))}
+      </div>
       <StudentManager />
     </div>
   );
